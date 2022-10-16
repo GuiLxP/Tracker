2f0c148ea93c36557d30b4e33f88785f1a8c17fb
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-10-16 14:59:34 -0300

    .

--- src/components/Cronometro.vue
+++ src/components/Cronometro.vue
@@ -9,7 +9,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-  name: "Cronometro",
+  name: "Cronometro-",
   props: {
     tempoEmSegundos: {
       type: Number,
