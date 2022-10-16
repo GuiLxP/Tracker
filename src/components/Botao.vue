835ba96444c362b6d18da8775914207b8fc66d98
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-10-16 14:52:35 -0300

    Interação com listas funcionando. Projeto quase finalizado.

--- src/components/Botao.vue
+++ src/components/Botao.vue
@@ -10,7 +10,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 export default defineComponent({
-  name: 'Botao',
+  name: 'Botao-',
   emits: [ 'clicado' ],
   props: {
     desabilitado: {
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -16,6 +16,7 @@ import { defineComponent } from 'vue';
 import Temporizador from './Temporizador.vue';
 export default defineComponent({
   name: 'Formulario-Nome',
+  emits: [ 'aoSalvarTarefa' ],
   components: {
     Temporizador
   },
@@ -26,8 +27,10 @@ export default defineComponent({
   },
   methods: {
     finalizarTarefa(tempoDecorrido: number): void {
-      console.log('tempo da tarefa', tempoDecorrido)
-      console.log('Descricao da tarefa', this.descricao)
+      this.$emit('aoSalvarTarefa', {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao
+      })
       this.descricao = ''
     }
   }
