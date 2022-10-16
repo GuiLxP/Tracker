2ad3565e8a312c5057e3b9d46c86b3b4ad68954a
Guilherme Pantoja <guilhermematheus166@gmail.com>
2022-10-16 15:22:24 -0300

    Pronto para adicionar o modo escuro.

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,12 @@
       <BarraLateral />
     </div>
     <div class="column is-three-quarter">
-      <Formulario @aoSalvarTarefa="salvarTarefa"/>
+      <Formulario @aoSalvarTarefa="salvarTarefa" />
       <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Box v-if="listaEstaVazia">
+          Você não esta muito produtivo hoje =(
+        </Box>
       </div>
     </div>
   </main>
@@ -18,22 +21,29 @@ import BarraLateral from './components/BarraLateral.vue';
 import Formulario from './components/Formulario.vue';
 import Tarefa from './components/Tarefa.vue'
 import ITarefa from './interfaces/ITarefa'
+import Box from './components/Box.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
     Formulario,
-    Tarefa
+    Tarefa,
+    Box
   },
-  data () {
+  data() {
     return {
       tarefas: [] as ITarefa[]
     }
   },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
+    salvarTarefa(tarefa: ITarefa) {
       this.tarefas.push(tarefa)
+    }
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
     }
   }
 
